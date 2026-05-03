62d6ede6ac9c5c7b8027741b7ae2fe4fc47e848d
shamilanazIT-QA <shamilanaz9@gmail.com>
2026-05-03 16:36:23 -0500

    Updated test_windows_pom.py

--- tests/test_windows_pom.py
+++ tests/test_windows_pom.py
@@ -7,7 +7,7 @@ from pages.window_page import WindowPage
 
 def test_multi_window():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         context= browser.new_context()
         page= context.new_page()
         # Use the Page Object
